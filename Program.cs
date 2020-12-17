43beb9cd44fb4ad97a9475a996933a3d6050e3ec
suriyasivakumar <surya@baimaas.com>
2020-12-17 09:14:55 +0530

    added by suriya

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Khadhar sheik");
+            Console.WriteLine("Khadhar sheik & suriya");
         }
     }
 }
